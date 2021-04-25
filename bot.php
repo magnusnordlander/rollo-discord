11daaab0d248dc9be7e6c773ef17e84c35bc624a
Magnus Nordlander <magnus@nordlander.se>
2021-04-25 15:29:54 +0200

    Pretty much working

--- bot.php
+++ bot.php
@@ -3,17 +3,35 @@
 include __DIR__.'/vendor/autoload.php';
 
 use Discord\Discord;
+use Discord\Parts\Channel\Message;
+use Fervo\Rollo\DiceExpressionParser;
+use Fervo\Rollo\DieInterface;
+use Symfony\Component\Dotenv\Dotenv;
+
+$dotenv = new Dotenv();
+$dotenv->loadEnv(__DIR__.'/.env');
+
+$parser = new DiceExpressionParser();
 
 $discord = new Discord([
-    'token' => getenv('DISCORD_BOT_TOKEN'),
+    'token' => $_ENV['DISCORD_BOT_TOKEN'],
 ]);
 
-$discord->on('ready', function ($discord) {
+$discord->on('ready', function (Discord $discord) use ($parser) {
     echo "Bot is ready!", PHP_EOL;
 
     // Listen for messages.
-    $discord->on('message', function ($message, $discord) {
-        echo "{$message->author->username}: {$message->content}",PHP_EOL;
+    $discord->on('message', function (Message $message, Discord $discord) use ($parser) {
+        if (preg_match("/^!roll (.*)/", $message->content, $matches)) {
+            try {
+                /** @var DieInterface $die */
+                $die = $parser->parseExpression($matches[1]);
+                $die->roll();
+                $message->reply(sprintf("%s rolled %s", $message->author->username, $die->getValueDescription()));
+            } catch (\Exception $e) {
+                $message->reply("I'm sorry, what now? ".get_class($e).': '.$e->getMessage());
+            }
+        }
     });
 });
 
